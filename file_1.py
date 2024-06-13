d422235f91dfd06f017a2ecf24951bcf263e85f0
HirthikRahul <hirthikrahul@gmail.com>
2024-06-13 13:08:25 -0500

    made pblic and trying to import

--- file_1.py
+++ file_1.py
@@ -1,4 +1,7 @@
 print("hello")
 import pandas as pd
-#import pyspark
-df=pd.read_csv("test_1.csv")
+import pyspark
+from pyspark.sql import SparkSession
+a1=SparkSession.builder.appName('practice1').getOrCreate()
+#print(a1)
+df_pyspark=a1.read.csv("test1_cvs")
